713044d50f3766d4807d73e8658f24892b2ac179
Maryan G <maryangmzc@gmail.com>
2024-05-19 00:15:45 -0500

    Class from April 23

---
+++ Java/Abril_23/Test.java
@@ -0,0 +1,22 @@
+package Java.Abril_23;
+
+/*Nombre del paquete es importante y tampoco repetir nombre */
+
+/* LA CLASE DEBE LLAMARSE IGUAL QUE EL TEST */
+/*ES EL PROGRAMA INICIAL SIEMPRE SIGUE A ESTRUCTURA se ejecuta todo */
+
+public class Test { 
+
+    /*EL ARCHIVO Y LA CLASE TIENEN EL MISMO NOMBRE */
+
+
+    /*METODO MAIN ES EL PUBLIC ESTATIC VOID MAIN */ 
+    /*ES EL PUNTO DE ENTRADAS */
+    /*CLASES VAN EN MAYUSCULAS Y METODOS NO */
+    public static void main(String[] args) {
+        System.out.println("Hola a todos");
+        /*PARA IMPRIMIR EN PANTALLA */
+
+
+    }
+}
